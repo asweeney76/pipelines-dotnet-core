86c3705f488b1f61f6073c2deec7ce45c9d32bd3
adam sweeney <adam.sweeney@gmail.com>
2019-02-12 16:04:58 -0500

    add db support

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassLibrary1
+{
+    public class TestContext : DbContext
+    {
+            public TestContext(DbContextOptions<TestContext> options) : base(options)
+            {
+            }
+    
+            public DbSet<Config> Configs { get; set; }
+            
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Config>().ToTable("Config");
+            }
+    }
+}
